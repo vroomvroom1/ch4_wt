50845fa213ebd06adcb493b5e99f14dc1052b3ea
vroomvroom <mattvroom1@gmail.com>
2017-10-09 00:20:18 +0000

    done with section 4.4

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,7 @@
+# in app/controllers/movies_controller.rb
+
+def show
+  id = params[:id] # retrieve movie ID from URI route
+  @movie = Movie.find(id) # look up movie by unique ID
+  # will render app/views/movies/show.html.haml by default
+end
