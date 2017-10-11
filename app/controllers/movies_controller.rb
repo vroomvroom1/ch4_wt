3f90c21097dea05513435da4a7bf5a8f1f834b05
vroomvroom <mattvroom1@gmail.com>
2017-10-11 21:25:16 +0000

    Done with section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,8 +3,51 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+
+# in app/controllers/movies_controller.rb
+
+def show
+  id = params[:id] 
+  @movie = Movie.find(id) 
   
-  def new
-  # default: render 'new' template
+end
+
+
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
+  
+  
+def new
+  @movie = Movie.new
+  
+end 
+  
+def create
+  
+  @movie = Movie.create!(movie_params)  
+  flash[:notice] = "#{@movie.title} was successfully created."
+  redirect_to movies_path
+end
+
+def update
+  @movie = Movie.find params[:id]
+  
+  @movie.update_attributes!(movie_params)    
+  flash[:notice] = "#{@movie.title} was successfully updated."
+  redirect_to movie_path(@movie)
+end
+
+def edit
+  @movie = Movie.find params[:id]
+end
+
+def destroy
+  @movie = Movie.find(params[:id])
+  @movie.destroy
+  flash[:notice] = "Movie '#{@movie.title}' deleted."
+  redirect_to movies_path
+end
 end
