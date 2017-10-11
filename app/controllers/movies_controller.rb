5e10b11fac5e667eee6af6b281910671bc2507c5
vroomvroom <mattvroom1@gmail.com>
2017-10-11 20:34:17 +0000

    Done with section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
-# in app/controllers/movies_controller.rb
-
-def show
-  id = params[:id] # retrieve movie ID from URI route
-  @movie = Movie.find(id) # look up movie by unique ID
-  # will render app/views/movies/show.html.haml by default
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def new
+  # default: render 'new' template
+  end
 end
